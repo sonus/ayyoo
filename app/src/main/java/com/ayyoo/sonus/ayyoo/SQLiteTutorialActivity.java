043ac0e8604ada5f94a401cb9fd23377df60670c
Sonus Sundar <sonus.m.sundar@eqs.com>
2018-01-05 20:09:06 +0530

    first

---
+++ app/src/main/java/com/ayyoo/sonus/ayyoo/SQLiteTutorialActivity.java
@@ -0,0 +1,18 @@
+package com.ayyoo.sonus.ayyoo;
+
+
+import java.util.List;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class SQLiteTutorialActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
